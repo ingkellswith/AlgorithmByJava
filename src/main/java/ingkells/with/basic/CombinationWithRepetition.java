7776357c3cc7e6da6b48010a9fbd037f260c16a7
ingkellswith <allover3773@gmail.com>
2023-09-13 01:10:06 +0900

    조합, 순열, 중복 조합, 중복 순열

---
+++ src/main/java/ingkells/with/basic/CombinationWithRepetition.java
@@ -0,0 +1,44 @@
+package ingkells.with.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationWithRepetition {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int r = 2; // 중복 조합의 크기
+
+        List<List<Integer>> combinations = generateCombinations(arr, r);
+
+        for (List<Integer> combination : combinations) {
+            System.out.println(combination);
+        }
+    }
+
+    public static List<List<Integer>> generateCombinations(int[] arr, int r) {
+        List<List<Integer>> result = new ArrayList<>();
+        generateCombinations(arr, r, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void generateCombinations(int[] arr, int r, int index, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == r) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        if (index == arr.length) {
+            return;
+        }
+
+        // 현재 요소를 포함하는 경우
+        current.add(arr[index]);
+        generateCombinations(arr, r, index, current, result); // 같은 요소를 다시 선택
+        current.remove(current.size() - 1); // 백트래킹
+
+        // 현재 요소를 포함하지 않는 경우
+        generateCombinations(arr, r, index + 1, current, result); // 다음 요소로 넘어감
+    }
+}
+
---
+++ src/main/java/ingkells/with/basic/Permutation.java
@@ -0,0 +1,39 @@
+package ingkells.with.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutation {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int r = 2; // 순열의 크기
+
+        List<List<Integer>> permutations = generatePermutations(arr, r);
+
+        for (List<Integer> permutation : permutations) {
+            System.out.println(permutation);
+        }
+    }
+
+    public static List<List<Integer>> generatePermutations(int[] arr, int r) {
+        List<List<Integer>> result = new ArrayList<>();
+        generatePermutations(arr, r, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void generatePermutations(int[] arr, int r, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == r) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!current.contains(arr[i])) {
+                current.add(arr[i]);
+                generatePermutations(arr, r, current, result);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+}
