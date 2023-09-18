060aa175cf883b6f3b30f94c67c01f91c7e255f6
ingkells <allover3773@gmail.com>
2023-09-18 18:29:00 +0900

    백준 - 조합론 - 이항 계수 3 (페르마의 소정리) 더 나은 풀이

--- src/main/java/ingkells/with/baekjoon/combinatorics/BOJ11401.java
+++ src/main/java/ingkells/with/baekjoon/combinatorics/BOJ11401.java
@@ -1,65 +1,85 @@
 package ingkells.with.baekjoon.combinatorics;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
-
-
 // 백준 11401. 이항 계수 3
+// 원소 a가 어떤 정수이고 p가 소수인 경우, 페르마의 소정리에 의해 다음이 성립합니다.
+// a^p ≡ a (mod p)
+// a^(p-1) ≡ 1 (mod p)
+// 이 식에서 **^**는 거듭제곱을 나타냅니다.
+// 위의 식에서 a의 역원은 **a^(p-2)**와 같다는 것을 알 수 있습니다.
+// https://st-lab.tistory.com/241
+// 전체 풀이 참고
 public class BOJ11401 {
-    static final int MOD = 1_000_000_007;
 
+    final static long P = 1000000007;
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        long result = nCk(N, K);
-        bw.write(String.valueOf(result));
-        bw.newLine();
+        long N = Long.parseLong(st.nextToken());
+        long K = Long.parseLong(st.nextToken());
 
-        br.close();
-        bw.close();
+        // 분자 N!
+        long numer = factorial(N);
+
+        // 분모 (K! * (N-K)!) mod p
+        long denom = factorial(K) * factorial(N - K) % P;
+
+
+        // N! * 분모의 역원((K! * (N-K)!)
+        System.out.println(numer * pow(denom, P - 2) % P);
+
     }
 
-    // 빠른 거듭제곱을 사용하여 이항 계수를 계산하는 함수
-    static long nCk(int n, int k) {
-        if (k == 0 || n == k) {
-            return 1;
+
+    // 팩토리얼 함수
+    public static long factorial(long N) {
+        long fac = 1L;
+
+        while(N > 1) {
+            // 곱셈에서는 모듈러의 분배 법칙에 의해서 모듈러를 많이 계산해도 된다.
+            fac = (fac * N) % P;
+            N--;
         }
-
-        long[] fact = new long[n + 1];
-        fact[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            fact[i] = (fact[i - 1] * i) % MOD; // 팩토리얼 계산 및 모듈로 연산
-        }
-
-        // 원소 a가 어떤 정수이고 p가 소수인 경우, 페르마의 소정리에 의해 다음이 성립합니다.
-        // a^p ≡ a (mod p)
-        // a^(p-1) ≡ 1 (mod p)
-        // 이 식에서 **^**는 거듭제곱을 나타냅니다.
-        // 위의 식에서 a의 역원은 **a^(p-2)**와 같다는 것을 알 수 있습니다.
-
-        long denominator = (fact[k] * fact[n - k]) % MOD; // 분모 계산
-        long exponent = pow(denominator, MOD - 2); // 페르마의 소정리를 사용한 역원 계산
-        // long exponent = pow(denominator, MOD - 2) % MOD; // 이렇게 해도 정답처리
-        long result = (fact[n] * exponent) % MOD; // 최종 결과 계산
-
-        return result;
+        return fac;
     }
 
-    // 빠른 거듭제곱을 계산하는 함수
-    static long pow(long base, long exponent) {
-        long result = 1;
-        while (exponent > 0) {
-            if (exponent % 2 == 1) {
-                result = (result * base) % MOD;
-            }
-            base = (base * base) % MOD;
-            exponent /= 2;
+
+    /*
+     * 역원 구하는 함수
+     *
+     * base : 밑,   expo = 지수 (exponent)
+     *
+     * 거듭 제곱을 분할 정복 방식으로 푼다.
+     *
+     */
+    public static long pow(long base, long expo) {
+        // 지수가 1일 경우 base^1 이므로 base % P를 리턴
+        if(expo == 1) {
+            return base % P;
         }
-        return result;
+
+        // 지수의 절반에 해당하는 base^(expo / 2) 을 구한다.
+        long temp = pow(base, expo / 2);
+
+        /*
+         * 현재 지수가 홀 수 였다면
+         * base^(expo / 2) * base^(expo / 2) * base 이므로
+         * base를 한 번 더 곱해주어야 한다.
+         *
+         * ex) A^9 = A^4 * A^4 * A
+         */
+        if(expo % 2 == 1) {
+            return (temp * temp % P) * base % P;
+        }
+        return temp * temp % P;
+
     }
+
+
 }
